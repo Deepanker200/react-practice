5776e175e3534a647cbc1ecd2ef11677b9bae0b9
Deepanker Tiwari <deepamamazing@gmail.com>
2025-04-29 13:53:47 +0530

    React password generator continue..

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,6 +1,5 @@
 import { use, useState, useCallback } from "react"
 
-import './App.css'
 
 function App() {
   const [length, setLength] = useState(8)
@@ -27,8 +26,28 @@ function App() {
 
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
-
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className='flex shadow 
+    rounded-lg overflow-hidden mb-4'>
+          <input type="text" value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password" readOnly />
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x1">
+            <input 
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
